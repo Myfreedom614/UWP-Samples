a7a4b60a7723fee6f738940405138f4bd843d1ed
Franklin <myfreedom614@gmail.com>
2015-10-13 20:44:12 +0800

    Add ListBox ContextMenu UWP Sample for StackOverflow case:  http://stackoverflow.com/questions/33051372/how-to-add-listbox-item-contextmenu-in-uwp

---
+++ ListBoxContextMenuUWPApp1/ListBoxContextMenuUWPApp1/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ListBoxContextMenuUWPApp1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public List<TestClass> OC { get; set; }
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            OC = new List<TestClass>();
+            for(int i=1; i<=10; i++)
+            {
+                OC.Add(new TestClass() { Name = "Item" + i.ToString() });
+            }
+            this.DataContext = this;
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            var datacontext = (e.OriginalSource as FrameworkElement).DataContext;
+
+            //this datacontext is probably some object of some type T
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            var datacontext = (e.OriginalSource as FrameworkElement).DataContext;
+
+            //this datacontext is probably some object of some type T
+        }
+
+        private void Grid_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            FrameworkElement senderElement = sender as FrameworkElement;
+            FlyoutBase flyoutBase = FlyoutBase.GetAttachedFlyout(senderElement);
+            flyoutBase.ShowAt(senderElement);
+        }
+    }
+
+    public class TestClass
+    {
+        public string Name { get; set; }
+    }
+}
