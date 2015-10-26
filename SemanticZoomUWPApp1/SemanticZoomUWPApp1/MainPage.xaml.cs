931b17b73ff990c0521ba0faff5c2d8ea3a84dcc
Franklin <myfreedom614@gmail.com>
2015-10-26 17:15:47 +0800

    Hide Line under Group Header in ZoomedIn view

--- SemanticZoomUWPApp1/SemanticZoomUWPApp1/MainPage.xaml.cs
+++ SemanticZoomUWPApp1/SemanticZoomUWPApp1/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using SemanticZoomUWPApp1.ViewModel;
+﻿using SemanticZoomUWPApp1.Helper;
+using SemanticZoomUWPApp1.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -13,6 +14,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -27,6 +29,19 @@ namespace SemanticZoomUWPApp1
         {
             this.InitializeComponent();
             this.DataContext = new MainViewModel();
+        }
+
+        private void GetLiveZoom_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach (GridViewHeaderItem gvhi in VTHelper.FindVisualChildren<GridViewHeaderItem>((SemanticZoom)sender))
+            {
+                //Do something with GridViewHeaderItem here
+                foreach (Rectangle rec in VTHelper.FindVisualChildren<Rectangle>(gvhi))
+                {
+                    //Do something with Rectangle here
+                    rec.Height = 0;
+                }
+            }
         }
     }
 }
