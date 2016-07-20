446213d12e45af5e60cd8c26a49067429fd3a9bc
Franklin Chen <myfreedom614@gmail.com>
2016-07-20 11:25:16 +0800

    MyToolkit DataGrid Template sample for SO question: http://stackoverflow.com/questions/38459270/how-to-remove-the-header-of-mytoolkit-datagrid

---
+++ MyToolkitDataGridUWPApp1/MyToolkitDataGridUWPApp1/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace MyToolkitDataGridUWPApp1
+{
+    public class Person
+    {
+        public int Age { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Category { get; set; }
+        public string ImageUri { get; set; }
+    }
+}
