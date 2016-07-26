29a553f0ba509c780f38fd3f9aba549b8fcf7aae
Franklin Chen <myfreedom614@gmail.com>
2016-07-26 12:48:58 +0800

    Add Nearest Color WP8.1 Sample

---
+++ NearestColorWP81App1/NearestColorWP81App1/Helper/Vector3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NearestColorUWPApp1.Helper
+{
+    public class Vector3
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Z { get; set; }
+
+        public Vector3(float x, float y, float z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+        public static float Distance(Vector3 a, Vector3 b)
+        {
+            return (float)Math.Sqrt(Math.Pow(a.X - b.X, 2) + Math.Pow(a.Y - b.Y, 2) + Math.Pow(a.Z - b.Z, 2)); ;
+        }
+    }
+}
