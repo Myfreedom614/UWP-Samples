41b11ccc8b0da47b672480f344b4ae0d290c4c60
Franklin Chen <myfreedom614@gmail.com>
2016-09-27 01:59:30 +0800

    Update for ListView Template sampe

--- ListViewTemplateApp1/ListViewTemplateApp1/App.xaml.cs
+++ ListViewTemplateApp1/ListViewTemplateApp1/App.xaml.cs
@@ -72,7 +72,7 @@ namespace ListViewTemplateApp1
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                    rootFrame.Navigate(typeof(BlankPage1), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
---
+++ ListViewTemplateApp1/ListViewTemplateApp1/BlankPage1.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace ListViewTemplateApp1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class BlankPage1 : Page
+    {
+        public BlankPage1()
+        {
+            this.InitializeComponent();
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //Assign DataTemplate for selected items
+            foreach (var item in e.AddedItems)
+            {
+                ListViewItem _lvi = item as ListViewItem;
+                _lvi.ContentTemplate = (DataTemplate)this.Resources["dataTemplate1"];
+            }
+            //Remove DataTemplate for unselected items
+            foreach (var item in e.RemovedItems)
+            {
+                ListViewItem _lvi = item as ListViewItem;
+                _lvi.ContentTemplate = null;
+            }
+        }
+    }
+}
